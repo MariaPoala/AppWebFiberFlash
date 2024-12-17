d6035bb338da5453f6f6a294c48968c5bde72c3b
MariaPoala <maria.vilcapoma22@gmail.com>
2024-12-17 17:27:41 -0500

    cac

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import LayoutComponent from './shared/component/layout/layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () => import('./shared/component/layout/layout.component'),
+        children: [
+            {
+                path: 'dashboard',
+                loadComponent: () => import('./principal/dashboard/dashboard.component')
+            },
+            {
+                path:'',
+                redirectTo: 'dashboard',
+                pathMatch: 'full'
+            }
+        ]
+    },
+];
